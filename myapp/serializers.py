6cc0e7c7d0991bb1aef50cd59f29644c0df9770f
Crystal <chi200706@gmail.com>
2023-07-24 13:11:06 +0800

    update serializers

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,8 +1,15 @@
-# from myapp.models import User  # 我只撿了model裡面的一個小傢伙User
+from myapp.models import Account 
+from myapp.models import ItemKinds 
+
 from rest_framework import serializers
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
 
+class ItemKindsSerializer(serializers.ModelSerializer):
+    kind = AccountSerializer(many=True, read_only=True)
 
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = '__all__'
+    class Meta:
+        model = ItemKinds
+        fields = ['kind', 'desc']
